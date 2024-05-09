fd1d78cd2f2f57d907d180789d95796744711ccb
schnkumar762 <schnkumar762@gmail.com>
2024-05-09 10:59:09 +0530

    Update isprime.cpp

--- 1_Number theory /isprime.cpp
+++ 1_Number theory /isprime.cpp
@@ -1,1 +1,12 @@
-
+   int isPrime(int N){
+        // code here
+        int ans = 1;
+        if(N == 1)
+        return 0;
+        for(int i=2;i*i<=N;i++)
+        {
+            if(N % i == 0)
+            ans = 0;
+        }
+        return ans;
+    }
